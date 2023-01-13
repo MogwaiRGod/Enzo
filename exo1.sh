d386dd9263fbf3955f454da65602f91d1984b15a
MogwaiRGod <diane.roberg@gmail.com>
2023-01-13 22:09:26 +0100

    premier commit

---
+++ exo1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#fonction principale
+function interface () {
+	read -p "Entrer un nombre entier positif >>> " input;
+
+	#pour quitter l'application
+	[[ $input =~ [qQ] ]] && exit 0;
+
+	validiteEntree $input;
+	#si la valeur est invalide
+	if [[ $? = 1 ]]; then
+		echo "Valeur invalide"; sleep 1; interface;
+	else
+		afficherNombres;
+	fi
+}
+
+#vérifie l'entrée de l utilisateur
+function validiteEntree () {
+	#	0-> nombre entier 1-> n importe quoi d'autre
+	[[ $input =~ [^[:digit:]] ]] &&  return 1 || return 0;
+	return 2;
+}
+
+function afficherNombres () {
+	for (( i=0; i<=$input; i++)); do
+		echo -n $i" ";
+	done
+	echo;
+	return 3;
+}
+
+#################" LE SCRIPT
+
+while [ true ]; do
+	interface;
+done
+
+exit 0;
