30a16d3ac6fa0789d046f5409e74a8b0907b401b
MogwaiRGod <diane.roberg@gmail.com>
2023-01-14 15:46:45 +0100

    Màj exos précédents + exo 3; la vérification des entrées ne fonctionne pas

--- exo3.sh
+++ exo3.sh
@@ -2,39 +2,77 @@
 
 ### UTILISATION
 ## Entrer les nombres les uns après les autres; appuyer sur entrée pour valider chaque item
-## Appuyer sur (ENTREE) (champ vide) pour valider le tableau
+## Entrer (T) pour valider le tableau
+## Appuyer sur (ENTREE) (champ vide) pour quitter l'interface
 
 
 #fonction principale
+function initVariables (){
+	tab_uti=();
+	input=0;
+	return 0;
+}
+
 function interface () {
-	echo "Entrer un par un les nombres de votre tableau"
-	read -p "Entrer un nombre entier >>>	" input;
+	initVariables;
 
-	#pour quitter l'application
-	[[ $input = "" ]] && exit 0;
+	echo "Entrer un par un les nombres de votre tableau";
 
-	validiteEntree $input;
-	#si la valeur est invalide
-	if [[ $? = 1 ]]; then
-		echo "Valeur invalide"; sleep 1; interface;
-	else
-		afficherNombres;
-	fi
+	while !  [[ $input =~ [Tt] ]]; do
+		read -p "Entrer un nombre entier >>>	" input;
+
+		#pour quitter l'application
+		[[ $input = "" ]] && exit 0;
+
+		#pour valider le tableau
+		[[ $input =~ [Tt] ]] && break;
+
+
+		validiteEntree $input;
+		if [[ $? = 1 ]]; then
+			echo "Valeur invalide"; sleep 1; continue;
+		else
+			tab_uti+=($input);
+		fi
+	done
+
+	# si le tableau est validé mais est vide -> renvoyer une erreur
+	verifTab;
+	[[ $? = 1 ]] && interface || plusGdNbTab; echo $plus_grand;
+
 }
 
 #vérifie l'entrée de l utilisateur // peut-être un nombre négatif
 function validiteEntree () {
-	#	0-> nombre entier 1-> n importe quoi d'autre
-	[[ $input =~ [^[-]?^[:digit:]] ]] &&  return 1 || return 0;
+
+	#	0-> nombre entier positif ou négatif  1-> n importe quoi d'autre
+
+	tmp=$(echo $input | grep [[:alpha:]]);
+	if [[  $input =~ ^["-"?[:digit:]] ]]; then
+		[[ $tmp != "" ]] && return 1;
+		return 0;
+	else
+		return 1;
+	fi
 	return 2;
 }
 
-function afficherNombres () {
-	for (( i=0; i<=$input; i++)); do
-		echo -n $i" ";
+function plusGdNbTab () {
+	plus_grand=0;
+	for i in ${tab_uti[@]}; do
+		[ $i -gt $plus_grand ] && plus_grand=$i;
 	done
-	echo;
 	return 3;
+}
+
+function verifTab () {
+	if [[ ${#tab_uti[@]} = 0 ]]; then
+		echo "Le tableau est vide."; sleep 1;
+		return 1;
+	else
+		return 0;
+	fi
+	return 4;
 }
 
 #################" LE SCRIPT
