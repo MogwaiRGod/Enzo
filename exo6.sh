9783ce978925cb93dfe06fc67e161c2678f3357f
Diane <122520392+MogwaiRGod@users.noreply.github.com>
2023-01-15 23:23:51 +0100

    update comms

--- exo6.sh
+++ exo6.sh
@@ -16,7 +16,7 @@ function verifNb () {
 ### FONCTIONNEMENT
 # nous allons chercher le plus petit diviseur du nombre entré par l'utilisateur (il suffit qu il ait un diviseur autre que lui-même et 1 pour ne pas
 # être premier).
-# Pour cela, nous allons tester tous les nombres à partir de 2, puis uniquement les nombres pairs : en effet, s il est divisible par 2, il n'est pas premier,
+# Pour cela, nous allons tester tous les nombres à partir de 2, puis uniquement les nombres impairs : en effet, s il est divisible par 2, il n'est pas premier,
 # et s il n'est PAS divisible par 2, il ne sera forcément pas divisible par les autres nombres pairs, donc il est inutile de les tester.
 # Enfin, le dernier nombre dont on va tester la divisibilité est appriximativement <= la moitié du nombre entré par l'utilisateur : en effet, au de-là de la moitié de lui-même, 
 # le nombre ne peut plus se contenir lui-même un nombre entier de fois <=> il n'a plus de diviseurs à partir de là. Il est donc inutile de continuer la recherche.
