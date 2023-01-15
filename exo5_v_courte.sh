cc2bf9e60347c38fa1acefdb9501f465606f863e
MogwaiRGod <diane.roberg@gmail.com>
2023-01-15 16:19:13 +0100

    update exo 5 (chaînes identiques) + suppr v longue

--- exo5_v_courte.sh
+++ exo5_v_courte.sh
@@ -3,16 +3,18 @@
 ### UTILISATION
 ## Appuyer sur (ENTREE) (champ vide) pour quitter l'interface
 
-## pour savoir si deux chaînes sont des anagrammes, il suffit de leur enlever leurs caractèes spéciaux et chiffres
+## pour savoir si deux chaînes sont des anagrammes, il suffit de leur enlever leurs caractèes spéciaux, ponctuation et chiffres afin de ne garder que les lettres
 ## puis de trier toutes leurs lettres par ordre alphabétique.
 ## Si les chaînes obtenues sont identiques, alors ce sont bien des anagrammes.
+pas_anag="Ce ne sont pas des anagrammes.\n"
+
 function verifAnag () {
 	declare -a tab;
 	for i in "$@"; do
 		# commande qui trie les caractères des chaînes selon ce qui a été convenu plus haut
 		tab+=(`echo "$i" | sed -e 's.[[:punct:][:digit:]]..g' | tr '[:upper:]' '[:lower:]' | grep -o . | sort | tr -d "\n"`);
 	done
-	[[ ${tab[0]} != ${tab[1]} ]] && echo "Ce ne sont pas des anagrammes" || echo "Ce sont des anagrammes !";
+	[[ ${tab[0]} != ${tab[1]} ]] && echo -e $pas_anag || echo -e "Ce sont des anagrammes !\n";
 }
 
 function interface () {
@@ -24,6 +26,13 @@ function interface () {
 		[[ $chaine1 = "" ]] && exit 0;
 		read -p "Entrer votre seconde chaîne >>>	" chaine2;
 		[[ $chaine2 = "" ]] && exit 0;
+
+		#par définition, si les chaines sont identiques, ce ne sont pas des anagrammes
+		if [[ $chaine1 == $chaine2 ]]; then
+			echo -e $pas_anag;
+			interface;
+		fi
+
 
 		chaine1=$(echo $chaine1 |sed -e 's.[[:space:]]..g');
 		chaine2=$(echo $chaine2 |sed -e 's.[[:space:]]..g');
